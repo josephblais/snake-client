5bab3965983e4c6bb6e3799cb411c2f1379f2483
Joseph Blais <josephpierreblais@gmail.com>
2021-02-10 13:13:08 -0500

    linted

--- play.js
+++ play.js
@@ -4,7 +4,7 @@ const net = require('net');
  * Establishes connection with the game server
  */
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: '135.23.222.131',
     port: 50542
   });
@@ -16,7 +16,7 @@ const connect = function() {
   });
 
   return conn;
-}
+};
 
 console.log('Connecting ...');
 connect();
