3b2d5a84bf683098f12093f45013504944e3c03e
Joseph Blais <josephpierreblais@gmail.com>
2021-02-10 15:47:12 -0500

    snake now moves with WASD key commands

--- input.js
+++ input.js
@@ -1,16 +1,34 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  // stdin.on('data', handleUserInput(data));
+  process.stdin.on('data', handleUserInput);
   return stdin;
 };
 
-// const handleUserInput = function(input) {
-//   if (input === '\u0003') {
-//     process.exit();
-//   }
-// };
+const handleUserInput = function(keypress) {
+  if (keypress === '\u0003') {
+    process.exit();
+  }
+  if (keypress === "w") {
+    connection.write('Move: up');
+    console.log('Move: up');
+  }
+  if (keypress === "a") {
+    connection.write('Move: left');
+    console.log('left');
+  }
+  if (keypress === "s") {
+    connection.write('Move: down');
+    console.log('down');
+  } if (keypress === "d") {
+    connection.write('Move: right');
+    console.log('right');
+  }
+};
 
 module.exports =  { setupInput };
